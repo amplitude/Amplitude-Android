6460a8449e9bce4f5dead026c45ba35cd09150c0
Curtis Liu <curtis@amplitude.com>
2014-11-07 13:33:34 -0800

    Update to version 1.4.2

--- src/com/amplitude/api/Constants.java
+++ src/com/amplitude/api/Constants.java
@@ -2,7 +2,7 @@ package com.amplitude.api;
 
 public class Constants {
 
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4.2";
 
     public static final String EVENT_LOG_URL = "https://api.amplitude.com/";
 
