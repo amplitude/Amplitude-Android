346358a2493d2c71867aad2d373dd0a05e25ad5b
Daniel Jih <daniel@amplitude.com>
2017-07-05 16:16:52 -0700

    Version 2.14.0

--- src/com/amplitude/api/Constants.java
+++ src/com/amplitude/api/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String LIBRARY = "amplitude-android";
     public static final String PLATFORM = "Android";
-    public static final String VERSION = "2.13.4";
+    public static final String VERSION = "2.14.0";
 
     public static final String EVENT_LOG_URL = "https://api.amplitude.com/";
 
