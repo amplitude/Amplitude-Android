6172b540ef4353ba22ca4506e5a23864da1fa85f
Daniel Jih <daniel@amplitude.com>
2017-10-04 11:48:45 -0700

    Version 2.15.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## Unreleased
 
+## 2.15.0 (October 04, 2017)
+
 * Updating to latest version of OkHttp3 ([3.9.0](https://github.com/square/okhttp/blob/master/CHANGELOG.md#version-390))
 
 ## 2.14.1 (July 27, 2017)
--- src/com/amplitude/api/Constants.java
+++ src/com/amplitude/api/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String LIBRARY = "amplitude-android";
     public static final String PLATFORM = "Android";
-    public static final String VERSION = "2.14.1";
+    public static final String VERSION = "2.15.0";
 
     public static final String EVENT_LOG_URL = "https://api.amplitude.com/";
 
