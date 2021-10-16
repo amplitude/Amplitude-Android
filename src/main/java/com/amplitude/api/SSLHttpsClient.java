ecd974dcfc2d84939116f4a44b9fd7faa357e6a1
Dante Tam <dante.tam1@gmail.com>
2021-10-16 15:35:06 -0700

    Fix up PR

--- src/main/java/com/amplitude/api/SSLHttpsClient.java
+++ src/main/java/com/amplitude/api/SSLHttpsClient.java
@@ -1,27 +1,9 @@
 package com.amplitude.api;
 
-import android.util.Base64;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
-
 public class SSLHttpsClient extends HttpClient {
 
-    
+    public SSLHttpsClient(String apiKey, String url, String bearerToken) {
+        super(apiKey, url, bearerToken);
+    }
 
 }
