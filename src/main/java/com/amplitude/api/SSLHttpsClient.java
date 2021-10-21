c4c598f829ddb524f0eb767b06d965c5507507e1
Dante Tam <dante.tam1@gmail.com>
2021-10-21 13:59:55 -0700

    Commit SSL pinning code

--- src/main/java/com/amplitude/api/SSLHttpsClient.java
+++ src/main/java/com/amplitude/api/SSLHttpsClient.java
@@ -1,9 +1,163 @@
 package com.amplitude.api;
 
-public class SSLHttpsClient extends HttpClient {
+import android.util.Base64;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+class SSLHttpsClient extends HttpClient {
+
+    private static final String TAG = SSLHttpsClient.class.getName();
+    private static final AmplitudeLog logger = AmplitudeLog.getLogger();
+
+    private SSLSocketFactory sslSocketFactory;
+
+    private static final String CERTIFICATE_1 = ""
+            + "MIIGCDCCA/CgAwIBAgIQKy5u6tl1NmwUim7bo3yMBzANBgkqhkiG9w0BAQwFADCBhT"
+            + "ELMAkGA1UEBhMCR0IxGzAZBgNVBAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UE"
+            + "BxMHU2FsZm9yZDEaMBgGA1UEChMRQ09NT0RPIENBIExpbWl0ZWQxKzApBgNVBAMTIk"
+            + "NPTU9ETyBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTQwMjEyMDAwMDAw"
+            + "WhcNMjkwMjExMjM1OTU5WjCBkDELMAkGA1UEBhMCR0IxGzAZBgNVBAgTEkdyZWF0ZX"
+            + "IgTWFuY2hlc3RlcjEQMA4GA1UEBxMHU2FsZm9yZDEaMBgGA1UEChMRQ09NT0RPIENB"
+            + "IExpbWl0ZWQxNjA0BgNVBAMTLUNPTU9ETyBSU0EgRG9tYWluIFZhbGlkYXRpb24gU2"
+            + "VjdXJlIFNlcnZlciBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAI7C"
+            + "AhnhoFmk6zg1jSz9AdDTScBkxwtiBUUWOqigwAwCfx3M28ShbXcDow+G+eMGnD4LgY"
+            + "qbSRutA776S9uMIO3Vzl5ljj4Nr0zCsLdFXlIvNN5IJGS0Qa4Al/e+Z96e0HqnU4A7"
+            + "fK31llVvl0cKfIWLIpeNs4TgllfQcBhglo/uLQeTnaG6ytHNe+nEKpooIZFNb5JPJa"
+            + "XyejXdJtxGpdCsWTWM/06RQ1A/WZMebFEh7lgUq/51UHg+TLAchhP6a5i84DuUHoVS"
+            + "3AOTJBhuyydRReZw3iVDpA3hSqXttn7IzW3uLh0nc13cRTCAquOyQQuvvUSH2rnlG5"
+            + "1/ruWFgqUCAwEAAaOCAWUwggFhMB8GA1UdIwQYMBaAFLuvfgI9+qbxPISOre44mOzZ"
+            + "MjLUMB0GA1UdDgQWBBSQr2o6lFoL2JDqElZz30O0Oija5zAOBgNVHQ8BAf8EBAMCAY"
+            + "YwEgYDVR0TAQH/BAgwBgEB/wIBADAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUH"
+            + "AwIwGwYDVR0gBBQwEjAGBgRVHSAAMAgGBmeBDAECATBMBgNVHR8ERTBDMEGgP6A9hj"
+            + "todHRwOi8vY3JsLmNvbW9kb2NhLmNvbS9DT01PRE9SU0FDZXJ0aWZpY2F0aW9uQXV0"
+            + "aG9yaXR5LmNybDBxBggrBgEFBQcBAQRlMGMwOwYIKwYBBQUHMAKGL2h0dHA6Ly9jcn"
+            + "QuY29tb2RvY2EuY29tL0NPTU9ET1JTQUFkZFRydXN0Q0EuY3J0MCQGCCsGAQUFBzAB"
+            + "hhhodHRwOi8vb2NzcC5jb21vZG9jYS5jb20wDQYJKoZIhvcNAQEMBQADggIBAE4rdk"
+            + "+SHGI2ibp3wScF9BzWRJ2pmj6q1WZmAT7qSeaiNbz69t2Vjpk1mA42GHWx3d1Qcnyu"
+            + "3HeIzg/3kCDKo2cuH1Z/e+FE6kKVxF0NAVBGFfKBiVlsit2M8RKhjTpCipj4SzR7Jz"
+            + "sItG8kO3KdY3RYPBpsP0/HEZrIqPW1N+8QRcZs2eBelSaz662jue5/DJpmNXMyYE7l"
+            + "3YphLG5SEXdoltMYdVEVABt0iN3hxzgEQyjpFv3ZBdRdRydg1vs4O2xyopT4Qhrf7W"
+            + "8GjEXCBgCq5Ojc2bXhc3js9iPc0d1sjhqPpepUfJa3w/5Vjo1JXvxku88+vZbrac2/"
+            + "4EjxYoIQ5QxGV/Iz2tDIY+3GH5QFlkoakdH368+PUq4NCNk+qKBR6cGHdNXJ93SrLl"
+            + "P7u3r7l+L4HyaPs9Kg4DdbKDsx5Q5XLVq4rXmsXiBmGqW5prU5wfWYQ//u+aen/e7K"
+            + "JD2AFsQXj4rBYKEMrltDR5FL1ZoXX/nUh8HCjLfn4g8wGTeGrODcQgPmlKidrv0PJF"
+            + "GUzpII0fxQ8ANAe4hZ7Q7drNJ3gjTcBpUC2JD5Leo31Rpg0Gcg19hCC0Wvgmje3WYk"
+            + "N5AplBlGGSW4gNfL1IYoakRwJiNiqZ+Gb7+6kHDSVneFeO/qJakXzlByjAA6quPbYz"
+            + "Sf+AZxAeKCINT+b72x";
 
     public SSLHttpsClient(String apiKey, String url, String bearerToken) {
         super(apiKey, url, bearerToken);
     }
 
+    @Override
+    public HttpURLConnection getNewConnection(String url) throws IOException {
+        try {
+            URL urlObject = new URL(url);
+            HttpsURLConnection connection = (HttpsURLConnection) urlObject.openConnection();
+            connection.setSSLSocketFactory(getPinnedCertSslSocketFactory());
+            return connection;
+        } catch (ClassCastException | MalformedURLException e) {
+            throw new IllegalArgumentException("Invalid URL, must establish HTTPS connection on PinnedAmplitudeClient for SSL features");
+        }
+    }
+
+    /**
+     * Gets pinned cert ssl socket factory.
+     *
+     * @return the pinned cert ssl socket factory
+     */
+    protected SSLSocketFactory getPinnedCertSslSocketFactory() {
+        return getPinnedCertSslSocketFactory(SSL_CONTEXT_API_AMPLITUDE_COM);
+    }
+
+    /**
+     * Gets pinned cert ssl socket factory.
+     *
+     * @param context the context
+     * @return the pinned cert ssl socket factory
+     */
+    protected SSLSocketFactory getPinnedCertSslSocketFactory(SSLContextBuilder context) {
+        if (context == null) {
+            return null;
+        }
+        if (sslSocketFactory == null) {
+            try {
+                sslSocketFactory = context.build().getSocketFactory();
+                logger.i(TAG, "Pinning SSL session using Comodo CA Cert");
+            } catch (Exception e) {
+                logger.e(TAG, e.getMessage(), e);
+            }
+        }
+        return sslSocketFactory;
+    }
+
+    protected static final SSLContextBuilder SSL_CONTEXT_API_AMPLITUDE_COM =
+            new SSLContextBuilder().addCertificate(CERTIFICATE_1);
+
+    /**
+     * SSl context builder, used to generate the SSL context.
+     */
+    protected static class SSLContextBuilder {
+        private final List<String> certificateBase64s = new ArrayList<String>();
+
+        /**
+         * Add certificate ssl context builder.
+         *
+         * @param certificateBase64 the certificate base 64
+         * @return the ssl context builder
+         */
+        public SSLContextBuilder addCertificate(String certificateBase64) {
+            certificateBase64s.add(certificateBase64);
+            return this;
+        }
+
+        /**
+         * Build ssl context.
+         *
+         * @return the ssl context
+         */
+        public SSLContext build() {
+            try {
+                CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                        TrustManagerFactory.getDefaultAlgorithm());
+                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                keyStore.load(null, null); // Use a null input stream + password to create an empty key store.
+
+                // Decode the certificates and add 'em to the key store.
+                int nextName = 1;
+                for (String certificateBase64 : certificateBase64s) {
+                    byte[] decodedCert = Base64.decode(certificateBase64, Base64.DEFAULT);
+                    InputStream stream = new ByteArrayInputStream(decodedCert);
+                    X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(stream);
+                    keyStore.setCertificateEntry(Integer.toString(nextName++), certificate);
+                }
+
+                // Create an SSL context that uses these certificates as its trust store.
+                trustManagerFactory.init(keyStore);
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+                return sslContext;
+            } catch (GeneralSecurityException | IOException e) {
+                logger.e(TAG, e.getMessage(), e);
+            }
+            return null;
+        }
+    }
 }
--- src/test/java/com/amplitude/api/PinningTest.java
+++ src/test/java/com/amplitude/api/PinningTest.java
@@ -35,5 +35,106 @@ public class PinningTest extends BaseTest {
         super.tearDown();
         PinnedAmplitudeClient.instances.clear();
     }
-    
+
+    @Test
+    public void testSslPinning() {
+        amplitude = PinnedAmplitudeClient.getInstance();
+        amplitude.initialize(context, "1cc2c1978ebab0f6451112a8f5df4f4e");
+        ShadowLooper looper = Shadows.shadowOf(amplitude.logThread.getLooper());
+        looper.runOneTask();
+        looper.runOneTask();
+        amplitude.httpService = amplitude.initHttpService();
+
+        amplitude.logEvent("pinned_test_event", null);
+        looper.runToEndOfTasks();
+        looper.runToEndOfTasks();
+
+        ShadowLooper httplooper = Shadows.shadowOf(amplitude.httpService.getHttpThreadLooper());
+        httplooper.runToEndOfTasks();
+
+        looper.runToEndOfTasks();
+
+        assertEquals(getUnsentEventCount(), 0);
+    }
+
+    @Test
+    public void testSslPinningInvalid() {
+        amplitude = PinnedAmplitudeClient.getInstance();
+        amplitude.initialize(context, "1cc2c1978ebab0f6451112a8f5df4f4e");
+
+        ShadowLooper looper = Shadows.shadowOf(amplitude.logThread.getLooper());
+        looper.runToEndOfTasks();
+
+        amplitude.httpService.messageHandler.httpClient = new InvalidSSLHttpsClient();
+
+        amplitude.logEvent("pinned_test_event_invalid", null);
+        looper.runToEndOfTasks();
+        looper.runToEndOfTasks();
+
+        ShadowLooper httplooper = Shadows.shadowOf(amplitude.httpService.getHttpThreadLooper());
+        httplooper.runToEndOfTasks();
+
+        assertEquals(getUnsentEventCount(), 1);
+    }
+
+    @Test
+    public void testInvalidHttpUrl() {
+        amplitude = PinnedAmplitudeClient.getInstance();
+        amplitude.setServerUrl("http://api.amplitude.com"); //note HTTP not HTTPS
+        amplitude.initialize(context, "1cc2c1978ebab0f6451112a8f5df4f4e");
+
+        ShadowLooper looper = Shadows.shadowOf(amplitude.logThread.getLooper());
+        looper.runToEndOfTasks();
+
+        amplitude.logEvent("event_should_never_send", null);
+        looper.runToEndOfTasks();
+        looper.runToEndOfTasks();
+
+        ShadowLooper httplooper = Shadows.shadowOf(amplitude.httpService.getHttpThreadLooper());
+        httplooper.runToEndOfTasks();
+
+        assertEquals(getUnsentEventCount(), 1);
+    }
+
+    private static class InvalidSSLHttpsClient extends SSLHttpsClient {
+        public static final SSLHttpsClient.SSLContextBuilder INVALID_SSL_CONTEXT = new SSLHttpsClient.SSLContextBuilder()
+          .addCertificate(""
+              + "MIIFVjCCBD6gAwIBAgIRAObsedhCFsMHaYL156gA4XAwDQYJKoZIhvcNAQELBQAwgZ"
+              + "AxCzAJBgNVBAYTAkdCMRswGQYDVQQIExJHcmVhdGVyIE1hbmNoZXN0ZXIxEDAOBgNV"
+              + "BAcTB1NhbGZvcmQxGjAYBgNVBAoTEUNPTU9ETyBDQSBMaW1pdGVkMTYwNAYDVQQDEy"
+              + "1DT01PRE8gUlNBIERvbWFpbiBWYWxpZGF0aW9uIFNlY3VyZSBTZXJ2ZXIgQ0EwHhcN"
+              + "MTQwODE0MDAwMDAwWhcNMTkwODEzMjM1OTU5WjBcMSEwHwYDVQQLExhEb21haW4gQ2"
+              + "9udHJvbCBWYWxpZGF0ZWQxHTAbBgNVBAsTFFBvc2l0aXZlU1NMIFdpbGRjYXJkMRgw"
+              + "FgYDVQQDFA8qLnlpa3lha2FwaS5uZXQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwgg"
+              + "EKAoIBAQDANfb+9W5g48LTQezWOMdQlL6kE66mqAnR9GAM1Ron31WuHQFn52Y/A6KK"
+              + "EfUUHIcC/3vgLHRGzWlzPs8ctTHIMH++Tb2eS3uNhyeiQ2ZTALYFslNThZsdoh/kYu"
+              + "YD5qX55ZKP1DJxm2ftcR38XoyWH1mv/JsT1Hq6/ATsesJJxwzxjI2G4NZyPFm8c2w8"
+              + "EhfdBgDGyBliGo24TpM7uOVYmC01mAB/pZZS2EuBWjhA6Ny7pTLnjIBAx3jh8Vd3is"
+              + "cMxq5boq2DKD0rSVQWbWdYbFMvvMmvq8qnuX9IijqbykoHoHGKerE/LaDs3xDZTlpE"
+              + "AvAt8oFUyAllUNCairq3AgMBAAGjggHcMIIB2DAfBgNVHSMEGDAWgBSQr2o6lFoL2J"
+              + "DqElZz30O0Oija5zAdBgNVHQ4EFgQUMpYvhtltmd1BhVilRheMd6wqENYwDgYDVR0P"
+              + "AQH/BAQDAgWgMAwGA1UdEwEB/wQCMAAwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQ"
+              + "UFBwMCMFAGA1UdIARJMEcwOwYMKwYBBAGyMQECAQMEMCswKQYIKwYBBQUHAgEWHWh0"
+              + "dHBzOi8vc2VjdXJlLmNvbW9kby5uZXQvQ1BTMAgGBmeBDAECATBUBgNVHR8ETTBLME"
+              + "mgR6BFhkNodHRwOi8vY3JsLmNvbW9kb2NhLmNvbS9DT01PRE9SU0FEb21haW5WYWxp"
+              + "ZGF0aW9uU2VjdXJlU2VydmVyQ0EuY3JsMIGFBggrBgEFBQcBAQR5MHcwTwYIKwYBBQ"
+              + "UHMAKGQ2h0dHA6Ly9jcnQuY29tb2RvY2EuY29tL0NPTU9ET1JTQURvbWFpblZhbGlk"
+              + "YXRpb25TZWN1cmVTZXJ2ZXJDQS5jcnQwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLm"
+              + "NvbW9kb2NhLmNvbTApBgNVHREEIjAggg8qLnlpa3lha2FwaS5uZXSCDXlpa3lha2Fw"
+              + "aS5uZXQwDQYJKoZIhvcNAQELBQADggEBAEdw5iJwxvXcZlPQEbudu84VI48uwSYcGz"
+              + "xBVzOsfYPdLUc7HSYT8zwg2d5l89iLiapvHS6gQiASZRi7nzR/oqnARcVjWnvIKPlq"
+              + "+b3OUtNElnfSFXZnsgpxp3BlcCEfQs7faII89rTzxJqRf0fNo8Y4u3+k79zGF8xbon"
+              + "O8oXZt0ApxcYmFIQlhCddM20lgHLTeMx4yG5C2lHGJE3iUS7YVAq6ENRrgiVhcuf5R"
+              + "H1mWAYpFPJ7rOmpCReC6brxCho/7jg+fBqEUfCGyrMtYSRejCc9aZGBQmuz5v5iT6P"
+              + "XCBeVmjEX3kh4bkRPHJ5vyASNXUkF3nwVAe4cwOoLHN8o=");
+
+        public InvalidSSLHttpsClient() {
+            super("fake-key", Constants.EVENT_LOG_URL, null);
+        }
+
+        @Override
+        protected SSLSocketFactory getPinnedCertSslSocketFactory() {
+            return super.getPinnedCertSslSocketFactory(INVALID_SSL_CONTEXT);
+        }
+    }
 }
