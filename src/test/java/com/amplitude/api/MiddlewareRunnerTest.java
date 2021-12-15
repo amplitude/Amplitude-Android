46954f67477c2ce15ed2f02fb9b8d47363e8e0e6
yuhao900914 <yuhao900914@gmail.com>
2021-12-14 22:38:24 -0800

    test: isolate middlewareRunner test from amplitude client

--- src/test/java/com/amplitude/api/MiddlewareRunnerTest.java
+++ src/test/java/com/amplitude/api/MiddlewareRunnerTest.java
@@ -4,13 +4,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowLooper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -18,25 +14,11 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @Config(manifest= Config.NONE)
-public class MiddlewareRunnerTest extends BaseTest {
-
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        amplitude.initialize(context, apiKey);
-        Shadows.shadowOf(amplitude.logThread.getLooper()).runOneTask();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
+public class MiddlewareRunnerTest {
+    MiddlewareRunner middlewareRunner = new MiddlewareRunner();
 
     @Test
     public void testMiddlewareRun() throws JSONException {
-        ShadowLooper looper = Shadows.shadowOf(amplitude.logThread.getLooper());
-        looper.runToEndOfTasks();
-
         String middlewareDevice = "middleware_device";
         Middleware updateDeviceIdMiddleware = new Middleware() {
             @Override
@@ -49,11 +31,11 @@ public class MiddlewareRunnerTest extends BaseTest {
                 next.run(payload);
             }
         };
-        amplitude.addEventMiddleware(updateDeviceIdMiddleware);
+        middlewareRunner.add(updateDeviceIdMiddleware);
 
         JSONObject event = new JSONObject().put("device_model", "sample_device");
         MiddlewareExtra extra = new MiddlewareExtra();
-        boolean middlewareCompleted = amplitude.middlewareRunner.run(new MiddlewarePayload(event, (MiddlewareExtra) extra));
+        boolean middlewareCompleted = middlewareRunner.run(new MiddlewarePayload(event, (MiddlewareExtra) extra));
 
         assertTrue(middlewareCompleted);
         assertEquals(event.getString("device_model"), middlewareDevice);
@@ -61,9 +43,6 @@ public class MiddlewareRunnerTest extends BaseTest {
 
     @Test
     public void testRunWithNotPassMiddleware() throws JSONException {
-        ShadowLooper looper = Shadows.shadowOf(amplitude.logThread.getLooper());
-        looper.runToEndOfTasks();
-
         // first middleware
         String middlewareDevice = "middleware_device";
         Middleware updateDeviceIdMiddleware = new Middleware() {
@@ -90,11 +69,11 @@ public class MiddlewareRunnerTest extends BaseTest {
                 }
             }
         };
-        amplitude.addEventMiddleware(updateDeviceIdMiddleware);
-        amplitude.addEventMiddleware(swallowMiddleware);
+        middlewareRunner.add(updateDeviceIdMiddleware);
+        middlewareRunner.add(swallowMiddleware);
 
         JSONObject event = new JSONObject().put("device_model", "sample_device").put("user_id", "sample_user");
-        boolean middlewareCompleted = amplitude.middlewareRunner.run(new MiddlewarePayload(event));
+        boolean middlewareCompleted = middlewareRunner.run(new MiddlewarePayload(event));
 
         assertFalse(middlewareCompleted);
         assertEquals(event.getString("device_model"), middlewareDevice);
