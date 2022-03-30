b270f5a7df8719ef93ccebf5fdcc0e75f0495a68
Andrey Sokolov <falconandy@yandex.ru>
2022-03-30 19:33:10 +0400

    feat: Add versionId to tracking plan data

--- src/test/java/com/amplitude/api/PlanTest.java
+++ src/test/java/com/amplitude/api/PlanTest.java
@@ -20,12 +20,15 @@ public class PlanTest {
         String branch = "main";
         String version = "1.0.0";
         String source = "mobile";
+        String versionId = "9ec23ba0-275f-468f-80d1-66b88bff9529";
         testPlan.setBranch(branch)
                 .setSource(source)
-                .setVersion(version);
+                .setVersion(version)
+                .setVersionId(versionId);
         JSONObject result = testPlan.toJSONObject();
         assertEquals(branch, result.getString(Constants.AMP_PLAN_BRANCH));
         assertEquals(source, result.getString(Constants.AMP_PLAN_SOURCE));
         assertEquals(version, result.getString(Constants.AMP_PLAN_VERSION));
+        assertEquals(versionId, result.getString(Constants.AMP_PLAN_VERSION_ID));
     }
 }
